8d7f8e5d65fab94f4290a43a15fbd7a99a1f8caa
Wonk6677 <bbament@gmail.com>
2017-12-11 22:02:21 +0000

    Test

---
+++ Carputer.py
@@ -0,0 +1,10 @@
+import obd
+
+connection = obd.OBD("/dev/ttyUSB0") # create connection with USB 0
+
+cmd = obd.commands.RPM # select an OBD command (sensor)
+
+response = connection.query(cmd) # send the command, and parse the response
+
+print(response.value) # returns unit-bearing values thanks to Pint
+print(response.value.to("RPM")) # user-friendly unit conversions
